ee15e967be3ef51036d33a15f0636d4267457562
stacy867 <stacymurenzi@gmail.com>
2019-09-27 18:53:40 +0200

    added new classes

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin,current_user
 from . import login_manager
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -17,6 +18,8 @@ class Writer(UserMixin,db.Model):
     pass_secure = db.Column(db.String(255))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    blog=db.relationship('Blog',backref="writers")
+    comment=db.relationship('Comment',backref="writers")
     
     
     @property
@@ -35,3 +38,50 @@ class Writer(UserMixin,db.Model):
     
     def __repr__(self):
         return f'User {self.username}'
+    
+class Blog(db.Model):
+    __tablename__="blogposts"
+    
+    id= db.Column(db.Integer,primary_key = True) 
+    title= db.Column(db.String(255))
+    content= db.Column(db.String(255))
+    author=db.Column(db.String(255))
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    writer_id=db.Column(db.Integer,db.ForeignKey('writers.id'))
+    comment=db.relationship('Comment',backref="blogposts")  
+    
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    # @classmethod
+    # def clear_blogs(cls):
+    #     Blog.all_blogs.clear()
+        
+    # display blogs
+    
+    def get_blogs(id):
+        blogs = Blog.query.all()
+        
+        return blogs
+class Comment(db.Model):
+    __tablename__="comments"
+    
+    id = db.Column(db.Integer,primary_key =True)
+    feedback= db.Column(db.String(255))  
+    blog_id=db.Column(db.Integer,db.ForeignKey('blogposts.id'))
+    writer_id=db.Column(db.Integer,db.ForeignKey('writers.id'))
+    
+    
+    def save_comment(self):
+        '''
+        function that saves comments
+        '''
+        db.session.add(self)
+        db.session.commit() 
+        
+    @classmethod
+    def get_comments(self,id):
+        comment=Comment.query.filter_by(blog_id=id).all
+        return comment    
+               
