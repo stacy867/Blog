060701122292a6d5dbf7f62abb8d0b746e19348e
stacy867 <stacymurenzi@gmail.com>
2019-09-30 04:59:25 +0200

    tested writer model

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -4,10 +4,12 @@ from app.models import Writer
 class WriterModelTest(unittest.TestCase):
     
     def setUp(self):
-        self.new_writer = Writer(password ='hello')
-        
+        self.new_writer = Writer(password ='hello')     
     def test_password_setter(self):
         self.assertTrue(self.new_writer.pass_secure is not None)    
 
     def test_password_verification(self):
         self.assertTrue(self.new_writer.verify_password('hello'))
+
+         
+
